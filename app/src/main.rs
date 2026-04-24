cb475268a92a78c3be9e5976958451fce8e9e175
zzy170031-cmd <267238282+zzy170031-cmd@users.noreply.github.com>
2026-04-24 12:31:12 +0800

    desktop-shell: hide release console window

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use hope_app::{
     desktop_bridge::{DesktopInvokeRequest, DesktopInvokeResponse, invoke_desktop_command},
     ipc::{
